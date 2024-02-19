87295a7cf7faed6a3d073e1797f09282b8bdc2eb
João Antônio Barbosa <joaobarbosadev@gmail.com>
2024-02-18 22:28:45 -0300

    initial files

---
+++ init.lua
@@ -0,0 +1,30 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.backspace = "2"
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- use spaces for tabs and what no
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+-- Oil.nvim
+require("oil").setup()
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
