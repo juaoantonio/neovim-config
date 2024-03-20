9c705cd627b2a5e94779a22c40e51c3714629577
João Antônio Barbosa <joaobarbosadev@gmail.com>
2024-03-20 11:11:07 -0300

    delete <C-s> mapping for saving

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,6 +13,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.del("n", "<C-s>")
 
 -- Oil.nvim
 require("oil").setup()
